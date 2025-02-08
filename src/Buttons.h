3d97af907494d70133c897ec31fcef315fd3e0e5
Martin Heise <info@martinheise.de>
2025-02-08 18:21:58 +0100

    Buttons: support pullup mode with correct logic

--- src/Buttons.h
+++ src/Buttons.h
@@ -9,6 +9,12 @@
 using ButtonCallback = void (*)(byte id);
 
 /**
+ * input mode flag: reads low on button pressed, use e.g. with an external pullup resistor
+ * for INPUT_PULLUP mode (internal pullup resistor) implied automatically
+ */
+#define INVERTED_INPUT  0x80
+
+/**
  * simple push button, executing callback functions when the button is pressed
  * additional handles a boolean value, toggled whenever the button is pressed
  */
@@ -40,6 +46,7 @@ private:
     uint8_t pin;
     bool pressed{false}; // debounced pressed state
     bool value{false};
+    int _pressedRead{LOW}; // expected read value for presses state
     ButtonCallback _clickCallback = nullptr;
     ButtonCallback _longClickCallback = nullptr;
     byte _callbackId{0};
